cbf28611dd47485010bb670d385fbf1bea0b7a0e
mendezdpatricia <mendez.dpatricia@gmail.com>
2025-08-21 21:51:44 -0600

    Split data in train, dev and test sets. Option to include title or not as part of the training. PyTorch dataset class.

--- src/data_prep.py
+++ src/data_prep.py
@@ -5,7 +5,12 @@ import os
 import re
 import string
 
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
+
 from sklearn.model_selection import train_test_split
+
 
 def download_data():
 
@@ -20,11 +25,11 @@ def download_data():
 	fake_df["label"] = 0
 
 	df = pd.concat([real_df, fake_df], axis=0).reset_index(drop=True)
-	df.dropna()
 
 	return df 
 
-def preprocess_subject(df):
+
+def preprocess_subject(df, include_title=True):
 
 	print("Custom subject preprocessing")
 
@@ -39,25 +44,53 @@ def preprocess_subject(df):
 	print('unique subjects: ', df['subject'].unique())
 	print('df dtypes: ', df.dtypes)
 
-	df["clean_text"] = (df["title"] + " " + df["text"]).apply(clean_text)
-	
+	df.dropna(subset=['title', 'text'], inplace=True)
+
+    # Decide what to clean
+    if include_title:
+    	df["clean_text"] = (df["title"] + " " + df["text"]).apply(clean_text)
+    else: 
+    	df["clean_text"] = df["text"].apply(clean_text)
+
+	# Remove rows where clean_text is empty
+	df = df[df['clean_text'].str.strip() != ""]
+
 	return df 
+
 
 def clean_text(text):
 
-    # lowercase
-    text = text.lower()
-
-    # no urls
-    text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
-
-    # remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
-
-    # remove numbers
-    text = re.sub(r'\d+', '', text)
-
-    # remove extra spaces
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = text.lower() # lowercase
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE) # no urls
+    text = text.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+    text = re.sub(r'\d+', '', text) # remove numbers
+    text = re.sub(r'\s+', ' ', text).strip() # remove extra spaces
 
     return text
+
+
+# Split into training, validation and test sets
+def split_data(texts, labels, extras):
+	# 70, 15, 15 split
+	train_texts, temp_texts, train_labels, temp_labels, train_extras, temp_extras = train_test_split(texts, labels, extras, test_size=0.3, random_state=42)
+	val_texts, test_texts, val_labels, test_labels, val_extras, test_extras = train_test_split(temp_texts, temp_labels, temp_extras, test_size=0.5, random_state=42)
+
+	return {'x_train': train_texts, 'y_train': train_labels, 'z_train': train_extras,
+			'x_test': test_texts, 'y_test': test_labels, 'z_test': test_extras,
+			'x_val': val_texts, 'y_val': val_labels, 'z_val': val_extras}
+
+
+# Create a PyTorch Dataset
+class NewsDataset(Dataset):
+
+	def __init__(self, encodings, labels):
+		self.encodings = encodings
+		self.labels = labels
+
+	def __len__(self):
+		return len(self.labels)
+
+	def __getitem__(self, idx):
+		item = {key: val[idx] for key, val in self.encodings.items()}
+		item['labels'] = torch.tensor(self.labels[idx])
+		return item
