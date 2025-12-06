30eaf67d618c78bf94f448e4cb7ad16cf4c27dfb
Diana Patricia Mendez <mendez.dpatricia@gmail.com>
2025-12-06 02:37:13 -0600

    Keep a test app file.

---
+++ app/api_only.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from inference import InferenceModel
+import os
+
+MODEL_1_DIR = "models/baseline_model"
+MODEL_2_DIR = "models/threshold_model"
+
+FALSE_LABEL_ID = 0  # LIKELY_FALSE
+
+app = FastAPI() # Initialize the FastAPI app
+
+class Claim(BaseModel):
+    text: str
+
+models = {
+    "baseline": InferenceModel(MODEL_1_DIR, "baseline", FALSE_LABEL_ID),
+    "thresholded": InferenceModel(MODEL_2_DIR, "thresholded", FALSE_LABEL_ID),
+}
+
+@app.get("/")
+def read_root():
+    return {"message": "API is running!"}
+
+# Prediction endpoint
+@app.post("/predict_all")
+def predict(claim: Claim):
+    """
+    claim: Claim  means the incoming JSON will be parsed into Claim(text="...")
+    """
+    results = {}
+
+    for name, model in models.items():
+        results[name] = model.predict(claim.text)
+
+    return results
